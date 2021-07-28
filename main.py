e5b83ee59902ee5e21c63104ea3ff567df3dfea9
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-07-28 14:42:33 +0700

    Add moving up/down for player

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.listen()
 screen.tracer(0)
+
+tim = Player()
+
+# Event handlers
+screen.onkey(tim.up, "Up")
+screen.onkey(tim.down, "Down")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
